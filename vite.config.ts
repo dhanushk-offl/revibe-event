60ca751c084d169f0124c2f3605728465f4c5c4b
dhanushk-offl <dhanushkandhan75@gmail.com>
2024-04-11 02:13:18 +0530

    10th commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,8 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  build: {
+    outDir: 'dist', // Change this to your desired output directory name
+  },
+  plugins: [sveltekit()],
 });
