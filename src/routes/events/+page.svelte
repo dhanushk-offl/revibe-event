6eec192cead79de92bff5ff9492f329e7509ce76
Dhanush K <128817931+dhanushk-offl@users.noreply.github.com>
2024-04-11 15:00:26 +0530

    Update Layout Box

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -6,11 +6,29 @@
 <svelte:head>
     <title>Revibe'24 Events</title>
 </svelte:head>
-<h2 class="text-lg font-sans font-bold text-center text-black">Technical Events Details</h2>
+<h2 class="flex flex-row flex-nowrap items-center mt-24">
+    <span class="flex-grow block border-t border-black"></span>
+    <span class="flex-none block mx-4 px-4 py-2.5 text-xl rounded leading-none font-medium bg-black text-white">
+        Technical Events
+    </span>
+    <span class="flex-grow block border-t border-black"></span>
+</h2>
 <TechEvents /><br>
-<h2 class="text-lg font-sans font-bold text-center text-black">Non Tech Events</h2>
+<h2 class="flex flex-row flex-nowrap items-center mt-24">
+    <span class="flex-grow block border-t border-black"></span>
+    <span class="flex-none block mx-4 px-4 py-2.5 text-xl rounded leading-none font-medium bg-black text-white">
+        Non - Technical Events
+    </span>
+    <span class="flex-grow block border-t border-black"></span>
+</h2>
 <NonTech />
-<h2 class="text-lg font-sans font-bold text-center text-black">Games</h2>
+<h2 class="flex flex-row flex-nowrap items-center mt-24">
+    <span class="flex-grow block border-t border-black"></span>
+    <span class="flex-none block mx-4 px-4 py-2.5 text-xl rounded leading-none font-medium bg-black text-white">
+        Games
+    </span>
+    <span class="flex-grow block border-t border-black"></span>
+</h2>
 <Games />
 <svelte:body></svelte:body>
 
