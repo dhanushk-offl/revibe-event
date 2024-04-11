f916306d3ccb8612d48b2ebdb1ec032641a5e9b3
Dhanush K <128817931+dhanushk-offl@users.noreply.github.com>
2024-04-11 15:07:41 +0530

    Update +page.svelte

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -15,14 +15,14 @@
     </span>
     <span class="flex-grow block border-t border-black"></span>
 </h2><br><br>
-<TechEvents /><br>
+<TechEvents />
 <h2 class="flex flex-row flex-nowrap items-center mt-24">
     <span class="flex-grow block border-t border-black"></span>
     <span class="flex-none block mx-4 px-4 py-2.5 text-xl rounded leading-none font-medium bg-black text-white">
         Non - Technical Events
     </span>
     <span class="flex-grow block border-t border-black"></span>
-</h2><br><br>
+</h2>
 <NonTech />
 <h2 class="flex flex-row flex-nowrap items-center mt-24">
     <span class="flex-grow block border-t border-black"></span>
@@ -30,7 +30,7 @@
         Games
     </span>
     <span class="flex-grow block border-t border-black"></span>
-</h2><br><br>
+</h2>
 <Games />
-<svelte:body></svelte:body>
+
 
