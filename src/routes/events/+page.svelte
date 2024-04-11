e7476aaa390590b81e400389d07315b2f2808b9e
Dhanush K <128817931+dhanushk-offl@users.noreply.github.com>
2024-04-11 14:51:18 +0530

    Head Style Update

--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -6,11 +6,11 @@
 <svelte:head>
     <title>Revibe'24 Events</title>
 </svelte:head>
-<h2>Technical Events Details</h2>
+<h2 class="text-lg font-sans font-bold text-center text-black">Technical Events Details</h2>
 <TechEvents /><br>
-<h2>Non Tech Events</h2>
+<h2 class="text-lg font-sans font-bold text-center text-black">Non Tech Events</h2>
 <NonTech />
-<h2>Games</h2>
+<h2 class="text-lg font-sans font-bold text-center text-black">Games</h2>
 <Games />
 <svelte:body></svelte:body>
 
