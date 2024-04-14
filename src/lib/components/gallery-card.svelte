c8df3aad7449e6eb4f634960e3349096b35b1b97
Dhanush K <128817931+dhanushk-offl@users.noreply.github.com>
2024-04-14 23:02:13 +0530

    Update gallery-card.svelte (5)

--- src/lib/components/gallery-card.svelte
+++ src/lib/components/gallery-card.svelte
@@ -5,31 +5,31 @@
         'mx-5 ms:w-36 ms:h-16 mm:w-48 mm:h-24 ml:w-56 ml:h-32 md:w-60 md:h-40 lg:w-72 lg:h-44 ds:w-80 dl:w-96 dl:h-52';
 </script>
 
-<div>
+<div class="relative z-50 mt-20"> <!-- Add relative positioning and z-index to the gallery container -->
     <div>
-        <div class="relative z-50 w-full ">
+        <div class="w-full ">
             <img class="w-full ds:h-[80px] " src="https://res.cloudinary.com/dtqdoinxc/image/upload/v1704810015/Revibe/Ellipse_2_sypezw.webp" alt="gallery-banner" />
         </div>
         <Marquee speed={70} class="flex flex-col z-10 mt-[-1rem] ms:mt-[-1rem]  md:mt-[-2rem] lg:mt-[-2.2rem] relative">
             {#each gallery as gallery}
-                <img class={pic_res} src={gallery.pic} alt={gallery.des} />
+                <img class={pic_res + ' mt-4'} src={gallery.pic} alt={gallery.des} /> <!-- Add margin-top to images -->
             {/each}
         </Marquee>
-        <div class="relative z-50 w-full mt-[-1rem] ms:mt-[-1rem] md:mt-[-2rem] lg:mt-[-2.2rem]">
+        <div class="w-full mt-[-1rem] ms:mt-[-1rem] md:mt-[-2rem] lg:mt-[-2.2rem]">
             <img class="w-full ds:h-[20px]" src="https://res.cloudinary.com/dtqdoinxc/image/upload/v1704810015/Revibe/Ellipse_2_sypezw.webp" alt="gallery-banner" />
         </div>
     </div>
 
     <div>
-        <div class="relative z-50 w-full ">
+        <div class="w-full ">
             <img class="w-full ds:h-[80px] " src="https://res.cloudinary.com/dtqdoinxc/image/upload/v1704810015/Revibe/Ellipse_2_sypezw.webp" alt="gallery-banner" />
         </div>
         <Marquee speed={70} direction="right" class="flex flex-col z-10 mt-[-1rem] ms:mt-[-1rem]  md:mt-[-2rem] lg:mt-[-2.2rem] relative">
             {#each gallery as gallery}
-                <img class={pic_res} src={gallery.pic} alt={gallery.des} />
+                <img class={pic_res + ' mt-4'} src={gallery.pic} alt={gallery.des} /> <!-- Add margin-top to images -->
             {/each}
         </Marquee>
-        <div class="relative z-50 w-full mt-[-1rem] ms:mt-[-1rem] md:mt-[-2rem] lg:mt-[-2.2rem]">
+        <div class="w-full mt-[-1rem] ms:mt-[-1rem] md:mt-[-2rem] lg:mt-[-2.2rem]">
             <img class="w-full ds:h-[80px]" src="https://res.cloudinary.com/dtqdoinxc/image/upload/v1704810015/Revibe/Ellipse_2_sypezw.webp" alt="gallery-banner" />
         </div>
     </div>
