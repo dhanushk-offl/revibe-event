5e3b8e86b2399349f0d7897288a108a9255eb7d1
Dhanush K <128817931+dhanushk-offl@users.noreply.github.com>
2024-04-14 22:58:31 +0530

    Update agenda-card.svelte BG image

--- src/lib/components/agenda-card.svelte
+++ src/lib/components/agenda-card.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { createEventDispatcher } from 'svelte';
   import type { Agenda } from '$lib/type';
+
   export let agenda: Agenda[];
-  import { clsx } from '$lib/utils';
+
   function redirectToRegister() {
-		window.location.href = '/events';
-	}
+    window.location.href = '/events';
+  }
 
   // Define a function to determine the background color based on category
   function getCategoryColor(category: string) {
@@ -16,27 +17,29 @@
   function showTicketsButton(category: string) {
     return category === 'tech' || category === 'non-tech' || category === 'games';
   }
+
   const dispatcher = createEventDispatcher();
 </script>
 
 <div class="py-1">
-    <div class="grid grid-rows md:grid-cols-2 ds:grid-cols-3 md:gap-5 gap-5">
-        {#each agenda as agenda}
-            <div class="relative bg-black md:w-[350px] mm:w-[310px] ml:w-[350px] w-[300px] h-[260px] text-white rounded-3xl p-7">
-               <div class="flex justify-between gap-6">
-                <div class="title text-xl font-display font-semibold">{agenda.title}</div>
-                <div class="category {getCategoryColor(agenda.category)} text-[10px] flex flex-col justify-center text-center font-display font-medium rounded-full w-[70px] h-[30px]">
-                    {agenda.category}
-                </div>
-               </div>
-                <div class="time py-5 font-display font-medium">{agenda.time}</div>
-                <div class="venue py-1 font-display font-medium">{agenda.venue}</div>
-                <div class="absolute bottom-0">
-                    {#if showTicketsButton(agenda.category)}
-                    <button class="action-button bg-white text-black px-4 py-2 rounded-md mb-8" on:click={redirectToRegister}>Get Your Tickets</button>
-                {/if}
-                </div>
-            </div>
-        {/each}
-    </div>
+  <div class="grid grid-rows md:grid-cols-2 ds:grid-cols-3 md:gap-5 gap-5">
+    {#each agenda as item}
+      <div class="relative bg-black md:w-[350px] mm:w-[310px] ml:w-[350px] w-[300px] h-[260px] text-white rounded-3xl p-7"
+           style="background-image: {item.image ? `url(${item.image})` : 'none'}">
+        <div class="flex justify-between gap-6">
+          <div class="title text-xl font-display font-semibold">{item.title}</div>
+          <div class="{getCategoryColor(item.category)} category text-[10px] flex flex-col justify-center text-center font-display font-medium rounded-full w-[70px] h-[30px]">
+            {item.category}
+          </div>
+        </div>
+        <div class="time py-5 font-display font-medium">{item.time}</div>
+        <div class="venue py-1 font-display font-medium">{item.venue}</div>
+        <div class="absolute bottom-0 mb-8"> <!-- Adjust margin-bottom here -->
+          {#if showTicketsButton(item.category)}
+            <button class="action-button bg-white text-black px-4 py-2 rounded-md" on:click={redirectToRegister}>Get Your Tickets</button>
+          {/if}
+        </div>
+      </div>
+    {/each}
+  </div>
 </div>
