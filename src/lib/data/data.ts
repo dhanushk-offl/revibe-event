076dde32fef7617873d44ba26a2dcb94dac58b58
Dhanush K <128817931+dhanushk-offl@users.noreply.github.com>
2024-04-17 17:28:43 +0530

    Update data.ts

--- src/lib/data/data.ts
+++ src/lib/data/data.ts
@@ -36,73 +36,73 @@ export const agenda: Agenda[] = [
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1712926375/person-front-computer-working-html_1_sbdwik.jpg"
     }, {
         title: "Tech Quiz",
-        time: "10:30 AM - 11:30 AM",
-        venue: "@Mechanical Seminar Hall",
+        time: "10:30 AM - 12:00 PM",
+        venue: "@MBA Seminar Hall",
         category: "tech",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1712930233/85313464_SL-050620-30640-05_1_tmzjor.jpg"
     }, {
         title: "Connections",
-        time: "11:00 AM - 12:30 PM",
+        time: "01:30 AM - 03:00 PM",
         venue: "@MBA Seminar Hall",
         category: "non-tech",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1713110932/connt_zebkic.png"
     }, {
         title: "Cooking without Fire",
-        time: "02:00 PM - 03:00 PM",
-        venue: "@Auditorium",
+        time: "11:00 AM - 12:30 PM",
+        venue: "@EEE Seminar Hall",
         category: "non-tech",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1713107714/cwf2_zvyjjf.png"
     },
     {
         title: "Photography",
-        time: "02:00 PM - 03:00 PM",
+        time: "10:30 AM - 02:00 PM",
         venue: "@Auditorium",
         category: "non-tech",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1712939082/photgraphy_ilvlzo.jpg"
     }, 
     {
         title: "Meme Creation",
-        time: "02:00 PM - 03:00 PM",
+        time: "10:30 AM - 02:00 PM",
         venue: "@Auditorium",
         category: "non-tech",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1712938215/meme_lvkoss.jpg"
     },
     {
         title: "Chess",
-        time: "02:30 PM - 03:30 PM",
-        venue: "@Auditorium",
+        time: "11:00 AM - 12:30 PM",
+        venue: "@Mechanical Block",
         category: "games",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1712937281/chess_qrzn4x.jpg"
     }, {
         title: "BGMI & Free Fire",
-        time: "02:00 PM - 03:00 PM",
-        venue: "@MBA Seminar Hall",
+        time: "01:30 PM - 03:00 PM",
+        venue: "@Auditorium",
         category: "games",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1712760425/bgmi-vs-freefire-max_dhoj6q.webp"
     }, {
         title: "Art(Painting & Pencil)",
-        time: "02:00 PM - 03:30 PM",
-        venue: "@Mechanical Seminar Hall",
+        time: "01:30 PM - 02:30 PM",
+        venue: "@Mechanical Block",
         category: "non-tech",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1712938470/art_zoiggj.jpg"
     },
     {
         title: "Act Like a GOAT",
-        time: "02:00 PM - 03:00 PM",
+        time: "01:30 PM - 03:00 PM",
         venue: "@Auditorium",
         category: "non-tech",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1712939622/goat_enso5i.jpg"
     },
     {
         title: "Gully Cricket",
-        time: "04:00 PM - 04:30 PM",
-        venue: "@Baseket Ball Arena - CAHCET",
+        time: "10:30 PM - 03:30 PM",
+        venue: "@Baseket Ball Arena",
         category: "games",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1712936329/gully_frdonm.jpg"
     },
     {
         title: "Vote of thanks",
-        time: "04:00 PM - 04:30 PM",
+        time: "03:40 PM - 04:30 PM",
         venue: "@Auditorium",
         category: "other",
         image:"https://res.cloudinary.com/dwir71gi2/image/upload/v1713106143/others_bg_pxrnqt.png"
