6e195cf7be6735d3641bce213143495f5b3e7d7b
Dhanush K <128817931+dhanushk-offl@users.noreply.github.com>
2024-04-21 12:32:45 +0530

    Update agenda-card.svelte

--- src/lib/components/agenda-card.svelte
+++ src/lib/components/agenda-card.svelte
@@ -33,7 +33,7 @@
         <div class="venue py-1 font-display font-medium">{item.venue}</div>
         <div class="absolute bottom-0 mb-8"> <!-- Adjust margin-bottom here -->
           {#if showTicketsButton(item.category)}
-            <a href="/events"><button class="action-button bg-white text-black px-4 py-2 rounded-md">About the Event</button></a>
+            <a href="/events"><button class="action-button bg-white text-black px-4 py-2 rounded-md" type="button">About the Event</button></a>
           {/if}
         </div>
       </div>
